562807bd460e93105a3a2f2352a3ebd21a5f99b0
Licinius <florent.dellomo@gmail.com>
2018-06-20 10:04:15 +0200

    Elle est où la poulette ?
    Suppression de l'avancement avec cookxml et material swing ui car c'est
    caca

--- src/view/ListFrame.java
+++ src/view/ListFrame.java
@@ -1,25 +1,4 @@
 package view;
-import mdlaf.*;
-import mdlaf.animation.*;
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
 
-import cookxml.cookswing.CookSwing;
-public class ListFrame {
-	
-	public static void main (String[] args) {
-		try {
-			UIManager.setLookAndFeel (new MaterialLookAndFeel ());
-		}
-		catch (UnsupportedLookAndFeelException e) {
-			e.printStackTrace ();
-		}
-		CookSwing cookSwing = new CookSwing ();
-		cookSwing.render("src"+File.separator+"view"+File.separator+"layout"+File.separator+"listframe.xml").setVisible(true);
-		JButton button =  (JButton) cookSwing.getId("the_button").object;
-
-		// on hover, button will change to a light blue
-		MaterialUIMovement.add (button, new Color (255, 255,255));
-	}
+public class ListFrame{
 }
