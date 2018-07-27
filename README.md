867ac3e58dd74a068e7ced82ad1c5647b1dc827d
Licinius <florent.dellomo@gmail.com>
2018-07-27 15:07:28 +0200

    reset

--- README.md
+++
@@ -1,3 +0,0 @@
-Keyword Analyse of the Dump Of staCkoverflow
-
-![alt text](https://vignette.wikia.nocookie.net/kaamelott-officiel/images/9/9d/Kadoc.jpg/revision/latest?cb=20151113170204&path-prefix=fr)
--- src/classification/SimilarityMeasure.java
+++
@@ -1,8 +0,0 @@
-package classification;
-
-import model.Question;
-
-public interface SimilarityMeasure<T> {
-	public double getSimiliratyMeasure(T a,T b);
-	public int getMostSimilarQuestion(Question question);
-}
